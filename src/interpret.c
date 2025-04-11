e1491de989301739e704f9c128ff8c684db2266c
YoAquinjs <sebascertuche@gmail.com>
2025-04-10 23:44:18 -0600

    add serialize_ast, serializes AST to graphviz DOT

--- src/interpret.c
+++ src/interpret.c
@@ -43,7 +43,7 @@ int decl_var(const char *id, union var_val val) {
     struct var_entry *free_entry = NULL;
     for (size_t i = 0; i < MAX_VAR_ENTRIES; i++) {
         if (strcmp(variables[i].id, id) == 0) {
-            sserror(false, "redeclaring '%s'\n", id);
+            sserror(false, "decl_var: redeclaring '%s'\n", id);
             return 1;
         }
 
@@ -52,7 +52,7 @@ int decl_var(const char *id, union var_val val) {
     }
 
     if (!free_entry) {
-        sserror(false, "max variables stored reached");
+        sserror(false, "decl_var: max variables stored reached");
         return 1;
     }
 
@@ -76,7 +76,7 @@ int decl_or_set_var(const char *id, var_type type, union var_val val) {
     }
 
     if (!free_entry) {
-        sserror(false, "max variables stored reached");
+        sserror(false, "decl_or_set_var: max variables stored reached");
         return 1;
     }
 
@@ -109,19 +109,19 @@ int set_var(const char *id, var_type type, union var_val val) {
         return 0;
     }
 
-    sserror(false, "assigning %s to variable of type %s\n", variables[type],
-            variables[var->type]);
+    sserror(false, "set_var: assigning %s to variable of type %s\n",
+            variables[type], variables[var->type]);
     return 1;
 }
 
 bool valid_id(const char *id) {
     if (!id) {
-        sserror(true, "null id string\n");
+        sserror(true, "valid_id: null id string\n");
         return false;
     }
 
     if (strlen(id) >= VAR_ID_SIZE) {
-        sserror(false, "id '%s' is too long\n", id);
+        sserror(false, "valid_id: id '%s' is too long\n", id);
         return false;
     }
 
@@ -155,10 +155,122 @@ union var_val default_var_val(var_type type) {
         return (union var_val)0;
 
     default:
-        sserror(true, "unknown type '%s' has no default value",
+        sserror(true, "default_var_val: unknown type '%s' has no default value",
                 vtype_names[type]);
         return (union var_val)0;
     }
+}
+
+const char *serialize_op(int operand) {
+    switch (operand) {
+    case '-':
+        return "-";
+    case '+':
+        return "+";
+    case '*':
+        return "*";
+    case '/':
+        return "/";
+    case '=':
+        return "=";
+    case VTYPE_INT:
+        return "int";
+    case VTYPE_FLOAT:
+        return "float";
+    default:
+        sserror(true, "operand_to_str: invalid opeand");
+        return NULL;
+    }
+}
+
+static int serialized_ntype_count[] = {
+    [NTYPE_ID] = 0,
+    [NTYPE_OP] = 0,
+    [NTYPE_CONST] = 0,
+};
+
+int serialize_ast_nodes(const struct ast_node *node, FILE *fout) {
+    if (!node)
+        return -1;
+    static const char *ntype_strid[] = {
+        [NTYPE_ID] = "id",
+        [NTYPE_OP] = "op",
+        [NTYPE_CONST] = "cst",
+    };
+
+    const char *label;
+    int type = node->type;
+    size_t node_id;
+
+    switch (type) {
+    case NTYPE_ID:
+    case NTYPE_OP:
+    case NTYPE_CONST:
+        node_id = serialized_ntype_count[type]++;
+        break;
+
+    default:
+        sserror(true, "serialize_ast_nodes: invalid node type");
+        return -1;
+    }
+
+    switch (type) {
+    case NTYPE_ID:
+        label = node->id.id;
+        break;
+
+    case NTYPE_OP:
+        label = serialize_op(node->op.op);
+        for (size_t i = 0; i < node->op.n_operands; i++) {
+            struct ast_node *operand = node->op.operands[i];
+            int operand_node_id = serialize_ast_nodes(operand, fout);
+            if (operand_node_id == -1) {
+                sserror(false, "serialize_ast_nodes: null operand in op_node");
+                continue;
+            }
+            fprintf(fout, "_op%lu -> _%s%lu\n", node_id,
+                    ntype_strid[operand->type], (size_t)operand_node_id);
+        }
+        break;
+
+    case NTYPE_CONST:
+        fprintf(fout, "_cst%lu [label=\"", node_id);
+        switch (node->cst.type) {
+        case VTYPE_INT:
+            fprintf(fout, "%d", node->cst.val.ival);
+            break;
+
+        case VTYPE_FLOAT:
+            fprintf(fout, "%f", node->cst.val.fval);
+            break;
+
+        default:
+            sserror(true, "serialize_ast_nodes: invalid cst_node vtype");
+            return -1;
+        }
+        fprintf(fout, "\"]\n");
+        return node_id;
+
+    default:
+        sserror(true, "serialize_ast_nodes: invalid node type");
+        return -1;
+    }
+
+    fprintf(fout, "_%s%lu [label=\"%s\"]\n", ntype_strid[type], node_id, label);
+    return node_id;
+}
+
+void serialize_ast(const struct ast_node *ast, FILE *fout) {
+    for (size_t i = 0; i < (sizeof(serialized_ntype_count) / sizeof(int));
+         i++) {
+        serialized_ntype_count[i] = 0;
+    }
+
+    fprintf(fout, "digraph Tree {\n");
+
+    serialize_ast_nodes(ast, fout);
+
+    fprintf(fout, "}\n");
 }
 
 void sserror(bool terminate, const char *s, ...) {
--- src/interpret.h
+++ src/interpret.h
@@ -4,11 +4,16 @@
 #include "parser.h"
 
 #include <stdbool.h>
+#include <stdio.h>
 
 #define VAR_ID_SIZE 10
 #define MAX_VAR_ENTRIES 26
 
 void initialize_interpreter(void);
+
+// serialize ast into graphviz representation
+void serialize_ast(const struct ast_node *ast, FILE *fout);
+
 void execute(const struct ast_node *node);
 
 void sserror(bool terminate, const char *s, ...);
