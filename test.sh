3b4ce14e2f34a6b86942cdcee7e1c93a62b1eabf
YoAquinjs <sebascertuche@gmail.com>
2025-04-13 20:28:28 -0600

    update test io

--- test.sh
+++ test.sh
@@ -13,10 +13,10 @@ usage() {
     echo "Returns: count of failed tests"
     echo
     echo "Each test consists of a directory test/ with:"
-    echo "  - optional args file containing command arguments"
+    echo "  - optional args file containing non io arguments"
     echo "  - input file passed to the binary by path"
     echo "  - expected file with the expected result"
-    echo "  - output.txt file the output of the last test execution"
+    echo "  - output file the output of the last test execution"
     echo
     echo "Example:"
     echo "  $0 ./build/bin tests/"
@@ -50,7 +50,7 @@ for test in "$tests_dir"/*/; do
     test_args=$(cat "$test/args" 2>/dev/null || true)
     test_input="$test/input"
     test_expected="$test/expected"
-    test_output="$test/output.txt"
+    test_output="$test/output"
 
     if [ ! -f "$test_input" ]; then
         echo "Skipping $testname: missing input file"
@@ -65,7 +65,7 @@ for test in "$tests_dir"/*/; do
     echo -n "Test $testname: "
 
     # execute program and discard control output
-    $program $test_args <"$test_input" >"$test_output" 2>/dev/null
+    $program $test_args "$test_input" "--output=$test_output" 2>/dev/null
 
     if diff=$(diff "$test_expected" "$test_output"); then
         echo "PASSED"
