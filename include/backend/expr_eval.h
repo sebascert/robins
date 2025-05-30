15912347790b2085b7fa4b1097a8dcc04692c6fa
Sebastian Certuche <sebascertuche@gmail.com>
2025-05-29 18:31:30 -0600

    Refactor translation for new syntax
    Refactor the translation process, dividing it into translator, and
    expression evaluation and arithmetic, the translation of an statement
    iterates over it's instructions, and in a line for each instruction
    outputs the mnemonic for each one, followed by the instruction
    arguments.
    
    The semantic validations are performed alongside the translation, when
    one ocurrs the statement parsing is stoped, work is pending on upgrading
    the error handling.

--- include/backend/expr_eval.h
+++ include/backend/expr_eval.h
@@ -1,16 +1,11 @@
 #ifndef BACKEND_EXPR_EVAL_H
 #define BACKEND_EXPR_EVAL_H
 
-#include "ast/nodes/literal.h"
+#include "ast/node.h"
 
-int cast_lit(struct literal *lit, literal_t cast);
-union literal_v literal_default(literal_t type);
+#include <stdbool.h>
 
-// arithmetic
-struct literal negate_lit(struct literal a);
-struct literal add_lit(struct literal a, struct literal b);
-struct literal sub_lit(struct literal a, struct literal b);
-struct literal mul_lit(struct literal a, struct literal b);
-struct literal div_lit(struct literal a, struct literal b);
+// translate AST to target
+struct literal expr_eval(const struct astnode *expr);
 
 #endif /* ifndef BACKEND_EXPR_EVAL_H */
--- src/backend/expr_eval.c
+++ src/backend/expr_eval.c
@@ -1,103 +1,81 @@
 #include "backend/expr_eval.h"
 
-int cast_lit(struct literal *lit, literal_t cast)
+#include "backend/arithmetic.h"
+#include "semantic/analyzer.h"
+
+struct literal op_unary_arithmetic(const struct operation *node);
+struct literal op_binary_arithmetic(const struct operation *node);
+
+struct literal expr_eval(const struct astnode *node)
 {
-    if (lit->type == cast)
-        return 0;
-
-    switch (lit->type) {
-        case LITERAL_T_INT:
-            if (cast == LITERAL_T_REAL) {
-                lit->val.rval = (float)lit->val.ival;
-                goto casted;
-            }
-            return 1;
-
-        case LITERAL_T_REAL:
-            if (cast == LITERAL_T_INT) {
-                lit->val.ival = (int)lit->val.rval;
-                goto casted;
-            }
-            return 1;
-
-        default:
-            return 1;
-    }
-casted:
-    lit->type = cast;
-    return 0;
-}
-
-union literal_v literal_default(literal_t type)
-{
-    switch (type) {
-        case LITERAL_T_INT:
-            return (union literal_v){.ival = 0};
-
-        case LITERAL_T_REAL:
-            return (union literal_v){.rval = 0.0f};
-
-        default:
-            return (union literal_v)0;
-    }
-}
-
-struct literal negate_lit(struct literal a)
-{
-    struct literal res = a;
-    if (a.type == LITERAL_T_INT) {
-        res.val.ival = -a.val.ival;
-    } else if (a.type == LITERAL_T_REAL) {
-        res.val.rval = -a.val.rval;
-    } else {
+    if (!node)
         return NULL_LITERAL;
-    }
 
-    return res;
+    switch (node->type) {
+        case ASTNODE_T_LITERAL:
+            return node->lit;
+        case ASTNODE_T_OPERATION:
+            switch (node->op.type) {
+                case '-':
+                    if (node->op.n_operands == 1) {
+                        goto unary;
+                    }
+                    goto binary;
+                case '+':
+                case '*':
+                case '/':
+                binary:
+                    if (node->op.n_operands != 2) {
+                        goto invalid_ast;
+                    }
+                    return op_binary_arithmetic(&node->op);
+                unary:
+                    if (node->op.n_operands != 1) {
+                        goto invalid_ast;
+                    }
+                    return op_unary_arithmetic(&node->op);
+                default:
+                    goto invalid_ast;
+            }
+        default:
+            break;
+    }
+invalid_ast:
+    return NULL_LITERAL;
 }
 
-struct literal add_lit(struct literal a, struct literal b)
+struct literal op_unary_arithmetic(const struct operation *node)
 {
-    if (a.type == LITERAL_T_REAL || b.type == LITERAL_T_REAL) {
-        float lhs = a.type == LITERAL_T_REAL ? a.val.rval : (float)a.val.ival;
-        float rhs = b.type == LITERAL_T_REAL ? b.val.rval : (float)b.val.ival;
-        return (struct literal){LITERAL_T_REAL, .val.rval = lhs + rhs};
-    } else {
-        return (struct literal){LITERAL_T_INT,
-                                .val.ival = a.val.ival + b.val.ival};
+    struct literal a;
+    a = expr_eval(node->operands[0]);
+    switch (node->type) {
+        case '-':
+            return negate_lit(a);
+        default:
+            return NULL_LITERAL;
     }
 }
 
-struct literal sub_lit(struct literal a, struct literal b)
+struct literal op_binary_arithmetic(const struct operation *node)
 {
-    if (a.type == LITERAL_T_REAL || b.type == LITERAL_T_REAL) {
-        float lhs = a.type == LITERAL_T_REAL ? a.val.rval : (float)a.val.ival;
-        float rhs = b.type == LITERAL_T_REAL ? b.val.rval : (float)b.val.ival;
-        return (struct literal){LITERAL_T_REAL, .val.rval = lhs - rhs};
-    } else {
-        return (struct literal){LITERAL_T_INT,
-                                .val.ival = a.val.ival - b.val.ival};
+    struct literal a, b, result;
+    a = expr_eval(node->operands[0]);
+    b = expr_eval(node->operands[1]);
+
+    switch (node->type) {
+        case '-':
+            return sub_lit(a, b);
+        case '+':
+            return add_lit(a, b);
+        case '*':
+            return mul_lit(a, b);
+        case '/':
+            result = div_lit(a, b);
+            if (result.type == LITERAL_T_NULL)
+                sserror("division by zero\n");
+
+            return result;
+        default:
+            return NULL_LITERAL;
     }
-}
-
-struct literal mul_lit(struct literal a, struct literal b)
-{
-    if (a.type == LITERAL_T_REAL || b.type == LITERAL_T_REAL) {
-        float lhs = a.type == LITERAL_T_REAL ? a.val.rval : (float)a.val.ival;
-        float rhs = b.type == LITERAL_T_REAL ? b.val.rval : (float)b.val.ival;
-        return (struct literal){LITERAL_T_REAL, .val.rval = lhs * rhs};
-    } else {
-        return (struct literal){LITERAL_T_INT,
-                                .val.ival = a.val.ival * b.val.ival};
-    }
-}
-
-struct literal div_lit(struct literal a, struct literal b)
-{
-    float lhs = a.type == LITERAL_T_REAL ? a.val.rval : (float)a.val.ival;
-    float rhs = b.type == LITERAL_T_REAL ? b.val.rval : (float)b.val.ival;
-
-    if (rhs == 0)
-        return NULL_LITERAL;
-    return (struct literal){LITERAL_T_REAL, .val.rval = lhs / rhs};
 }
